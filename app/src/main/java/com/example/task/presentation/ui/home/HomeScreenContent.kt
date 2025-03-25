d3f50cde6f7606aa1d19cf43a0a5194ad6da6d6d
KarimBahi <kbahi@dollargeneral.com>
2025-03-26 01:10:45 +0200

    minor enhancements

--- app/src/main/java/com/example/task/presentation/ui/home/HomeScreenContent.kt
+++ app/src/main/java/com/example/task/presentation/ui/home/HomeScreenContent.kt
@@ -5,8 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +16,10 @@ import androidx.compose.ui.unit.dp
 import com.example.task.R
 import com.example.task.common.components.ErrorDialog
 import com.example.task.common.components.LoadingDialog
+import com.example.task.common.components.MySpace4
+import com.example.task.common.utils.TestTags.CATS_LIST
+import com.example.task.common.utils.TestTags.CAT_ITEM
+import com.example.task.common.utils.TestTags.REFRESH_BUTTON
 import com.example.task.common.utils.noRippleClickable
 import com.example.task.domain.model.CatUIModel
 import com.example.task.presentation.ui.home.components.CatComponent
@@ -35,7 +38,7 @@ fun HomeScreenContent(
         }
 
         is UIState.Success -> {
-            SuccessComponent(stateValue.data, onRefresh)
+            CatsListComponent(stateValue.data, onRefresh)
         }
 
         is UIState.Error -> {
@@ -49,33 +52,38 @@ fun HomeScreenContent(
 }
 
 @Composable
-private fun SuccessComponent(
+private fun CatsListComponent(
     homeData: CatUIModel,
     onRefresh: () -> Unit
 ) {
-    val scrollState = rememberScrollState()
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .verticalScroll(scrollState)
-            .padding(MainMargin)
-            .testTag("cats_list"),
-        verticalArrangement = Arrangement.spacedBy(12.dp),
+            .padding(MainMargin),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.baseline_refresh_24),
             contentDescription = "Thumb Up",
-            modifier =
-            Modifier
-                .testTag("refresh_button")
+            modifier = Modifier
+                .testTag(REFRESH_BUTTON)
                 .noRippleClickable {
                     onRefresh.invoke()
                 },
         )
-
-        for (cat in homeData.cats) {
-            CatComponent(cat = cat)
+        MySpace4()
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .testTag(CATS_LIST),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(
+                homeData.cats.size,
+                key = { homeData.cats[it].id },
+                contentType = { _ -> CAT_ITEM }) {
+                CatComponent(cat = homeData.cats[it])
+            }
         }
     }
 }
@@ -83,7 +91,7 @@ private fun SuccessComponent(
 @Preview(name = "Home Preview", showBackground = true)
 @Composable
 private fun SuccessComponentPreview() {
-    SuccessComponent(
+    CatsListComponent(
         homeData = CatUIModel(cats = listOf()),
         onRefresh = { },
     )
